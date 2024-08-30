0e6070fa93ce59c9986b220ec00812665df3fec9
Yusuf Karakaya <yusufkarakaya92@gmail.com>
2024-08-30 14:51:33 -0700

    favicon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,11 @@ import { Inter, Poppins } from "next/font/google";
 import Head from "next/head"; // Import Head component
 import "./globals.css";
 
-const poppins = Poppins({ subsets: ["latin"], display: "swap", weight: ["400", "500", "600", "700"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400", "500", "600", "700"],
+});
 
 export const metadata: Metadata = {
   title: "Yusuf Karakaya",
@@ -18,10 +22,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" /> {/* Add favicon link */}
+        <link rel="icon" href="/icon.ico" sizes="any" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png" />
+        <link rel="mask-icon" href="/icon.svg" color="#000000" />
+
         {metadata.title && <title></title>}
-        {metadata.description && <meta name="description" content={metadata.description} />}
+        {metadata.description && (
+          <meta name="description" content={metadata.description} />
+        )}
       </Head>
+
       <body className={poppins.className}>{children}</body>
     </html>
   );
