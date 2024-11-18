2cb6b28a80959d00ec6a13ec9e40aa2dc1e2afa9
Yusuf Karakaya <yusufkarakaya92@gmail.com>
2024-11-17 21:03:33 -0800

    updated nav button design

--- app/navbar/page.jsx
+++ app/navbar/page.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import Link from 'next/link'
 import ShinyButton from '@/components/ui/shiny-button'
+import { Button } from '@/components/ui/button'
 
 const page = () => {
   return (
     <header className="flex justify-center gap-4 mb-8">
       <Link href="/">
-        <ShinyButton variant="outline">About</ShinyButton>
+        <Button variant="outline">About</Button>
       </Link>
+      -
       <Link href="/projects">
-        <ShinyButton variant="outline">Projects</ShinyButton>
+        <Button variant="outline">Projects</Button>
       </Link>
     </header>
   )
